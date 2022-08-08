da72c7774072ca211220ef5980723386304e8bd3
huatheechiang <huatheechiang@gmail.com>
2022-08-08 10:29:52 -0300

    second commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "Laikalilly1!",
+    password: "*********",
     database: "todo",
 });
 
